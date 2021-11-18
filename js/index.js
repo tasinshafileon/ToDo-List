66a35786dcf4c719ed44d369d8a96081eefafcc1
Tasin Shafi Leon <leontasinshafi@gmail.com>
2021-11-18 22:16:06 +0600

    changed the port address in order to listen to get request from other port address other than only listening to port 3000

--- js/index.js
+++ js/index.js
@@ -29,6 +29,6 @@ app.post("/", function(req, res){
 
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server is running on localhost:3000");
 });
