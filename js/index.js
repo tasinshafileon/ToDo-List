7f0381a0f640e09457cf5b60f027b882e4b3a9c7
Tasin Shafi Leon <leontasinshafi@gmail.com>
2021-11-27 18:31:26 +0600

    added node_modules to .gitignore

--- js/index.js
+++ js/index.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-var listItems = [];
+const mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static("public"));
 
 app.set("view engine", "ejs")
+
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const listItemsSchema = {
+  item: "String"
+};
+
+const ListItem = mongoose.model("item", listItemsSchema);
+
+
+
+// ListItem.insertMany([listItem1, listItem2, listItem3], function(err){
+//   if(err){
+//     console.log("There was an error");
+//   }else{
+//     console.log("Successfully inserted items");
+//   }
+// });
 
 app.get("/", function(req, res){
 
@@ -19,14 +37,23 @@ app.get("/", function(req, res){
     day: "numeric"
   };
 
-  res.render("index", {dayInfo: date.toLocaleDateString("en-US", options), items: listItems});
+  ListItem.find({}, function(err, foundItems){
+    res.render("index", {dayInfo: date.toLocaleDateString("en-US", options), items: foundItems});
+  });
+
 
 });
 
 app.post("/", function(req, res){
 
   if(req.body.userListItem!==""){
-    listItems.push(req.body.userListItem);
+
+    const listItem1 = new ListItem({
+      item: req.body.userListItem
+    });
+
+    listItem1.save();
+
   }
 
   res.redirect("/");
